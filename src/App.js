7816f24f6309892233fac5ac5c39d8cdc215b3e8
vankerok <vankerbv@gmail.com>
2024-01-24 17:15:28 -0700

    Files clean up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
-const a = 5
+import Hello from './components/check-wallet-balance.jsx' 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+      <Hello />
+    )
 }
 
 export default App;
--- src/components/check-wallet-balance.jsx
+++ src/components/check-wallet-balance.jsx
@@ -1,1 +1,10 @@
-const a = 2
+function Hello(){
+    return(
+        <div>
+            <h2>Hey</h2>
+            <p className='Greeting-Message'>Yo</p>
+        </div>
+    )
+}
+
+export default Hello;
